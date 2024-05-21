b29a94b4647c2dde3c1d3d23b448b0c18fd519ac
JCY-3958 <58291274+JCY-3958@users.noreply.github.com>
2024-05-21 17:53:49 +0900

    2024-05-21

---
+++ java/com/codeqna/controller/BoardApiController.java
@@ -0,0 +1,84 @@
+package com.codeqna.controller;
+
+import com.codeqna.dto.AddBoardRequest;
+import com.codeqna.dto.HeartDto;
+import com.codeqna.dto.ParentReplyDto;
+import com.codeqna.entity.Board;
+import com.codeqna.entity.Heart;
+import com.codeqna.service.BoardService;
+import com.codeqna.service.HeartService;
+import com.codeqna.service.ReplyService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/boardAPI")
+@RequiredArgsConstructor
+public class BoardApiController {
+
+    private final BoardService boardService;
+    private final ReplyService replyService;
+    private final HeartService heartService;
+
+    //게시물 등록
+    @PostMapping("/register")
+    public ResponseEntity<Board> addBoard(@RequestBody AddBoardRequest request){
+        Board savedBoard = boardService.save(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedBoard);
+    }
+
+    //게시물 검색
+    @GetMapping("/searchTable")
+    public List<Board> searchBoards(@RequestParam("condition") String condition,
+                                    @RequestParam("keyword") String keyword) {
+        return boardService.searchBoards(condition, keyword);
+    }
+
+
+    //게시물 삭제
+    @PutMapping("/delete/{bno}")
+    public ResponseEntity<Void> delete(@PathVariable Long bno) {
+        boardService.deleteBoard(bno);
+        System.out.println("요까지1");
+
+        return ResponseEntity.ok().build();
+    }
+
+    //좋아요를 눌렀을 경우
+    @PutMapping("/heart/like")
+    public ResponseEntity<Void> increaseHeart(@RequestBody HeartDto heartDto) {
+        heartService.increaseHeart(heartDto);
+        //System.out.println("닉네임 오냐? : " + heartDto.getNickname());
+
+        return ResponseEntity.ok().build();
+
+    }
+
+    //좋아요를 취소했을 경우
+    @PutMapping("/heart/unlike")
+    public ResponseEntity<Void> decreaseHeart(@RequestBody HeartDto heartDto) {
+        heartService.decreaseHeart(heartDto);
+        return ResponseEntity.ok().build();
+    }
+
+    //로그인 후 상세 페이지에 들어갔을 때 그 사용자가 그 게시물에 좋아요를 눌렀는지 여부 반환
+    @PostMapping("/heart/pressHeart")
+    public @ResponseBody Heart isHeart(@RequestBody HeartDto heartDto) {
+        System.out.println("이게 실행되나?");
+        System.out.println("이게 json : " + heartService.isHeart(heartDto));
+        return heartService.isHeart(heartDto);
+    }
+
+    //댓글 등록
+    @PostMapping("/comment/add")
+    public ResponseEntity<ParentReplyDto> addComment(@RequestBody ParentReplyDto parentReplyDto) {
+        replyService.addComment(parentReplyDto);
+        return ResponseEntity.ok().build();
+    }
+
+}
---
+++ java/com/codeqna/entity/Heart.java
@@ -0,0 +1,25 @@
+package com.codeqna.entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Heart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "hno", nullable = false)
+    private Long hno;
+
+    @Column(name = "nickname", nullable = false)
+    private String nickname;
+
+    @ManyToOne
+    @JoinColumn(name = "bno", nullable = false)
+    private Board board;
+}
